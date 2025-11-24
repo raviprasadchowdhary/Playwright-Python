aed01b3c4f040a21654a5f25d13656e25d455209
Raviprasad <78257726+raviprasadchowdhary@users.noreply.github.com>
2025-11-24 10:14:39 +0530

    oops

--- PythonBasics/oops.py
+++ PythonBasics/oops.py
@@ -4,20 +4,25 @@
 # this is a simple calculator class with basic operations
 class Calculator:
     def add(self, num1, num2):
-        return num1+num2
+        return num1 + num2
+
     def subtract(self, num1, num2):
-        return num1-num2
+        return num1 - num2
+
     def multiply(self, num1, num2):
-        return num1*num2
+        return num1 * num2
+
     def divide(self, num1, num2):
-        return num1/num2
+        return num1 / num2
+
 
 # this is how we create an object of a class
 calc = Calculator()
-print("Addition of 1 & 2 is: ", calc.add(1,2))
+print("Addition of 1 & 2 is: ", calc.add(1, 2))
 print("Substraction of 2 & 1 is: ", calc.subtract(2, 1))
-print("Multiplication of 2 & 3 is: ", calc.multiply(2,3))
+print("Multiplication of 2 & 3 is: ", calc.multiply(2, 3))
 print("Division of 6 & 2 is: ", calc.divide(6, 2))
+
 
 # Constructor in Python
 class Person:
@@ -29,6 +34,7 @@ class Person:
         print("Name: ", self.name)
         print("Age: ", self.age)
 
+
 # here we are creating an object of the Person class
 person1 = Person("Raviprasad", 25)
 person2 = Person("Alice", 21)
@@ -37,43 +43,53 @@ person2 = Person("Alice", 21)
 person1.display()
 person2.display()
 
+
 # Inheritance in Python
 # Base class
 class Animal:
     def speak(self):
         print("This animal makes a sound")
+
+
 # Derived class
 class Dog(Animal):
     def speak(self):
         print("This animal barks")
+
+
 # Derived class
 class Cat(Animal):
     def specialty(self):
         print("This animal is very agile")
+
 
 # creating objects of the derived classes
 dog = Dog()
 cat = Cat()
 
 # calling methods
-print("Dog-speak: ", dog.speak()) # Output: This animal barks
-print("Cat-speak: ", cat.speak()) # Output: This animal makes a sound
-print("Cat-specialty: ", cat.specialty()) # Output: This animal is very agile
+print("Dog-speak: ", dog.speak())  # Output: This animal barks
+print("Cat-speak: ", cat.speak())  # Output: This animal makes a sound
+print("Cat-specialty: ", cat.specialty())  # Output: This animal is very agile
+
 
 # Polymorphism in Python
 # polymorphism is the ability to use a common interface for multiple forms (data types).
 # creating two classes with the same method name
 class DogFeatures:
     def walk(self):
-        return str(self.__class__.__name__) , " walks on 4 legs"
+        return str(self.__class__.__name__), " walks on 4 legs"
+
 
 class BirdFeatures:
     def walk(self):
         return str(self.__class__.__name__), " walks on 2 legs"
 
+
 # function that takes an object and calls its fly method
 def animal_walk(animal):
     return animal.walk()
+
 
 # creating objects
 dog_features = DogFeatures()
@@ -82,6 +98,7 @@ bird_features = BirdFeatures()
 # calling the walk method on different objects
 print(animal_walk(dog_features))
 print(animal_walk(bird_features))
+
 
 # Encapsulation in Python
 # here we are creating a class with private attributes and methods
@@ -121,24 +138,27 @@ account2.withdraw(2600)
 print("Account2 balance after failed withdrawal: ", account2.get_balance())
 
 # trying to access private attribute will raise an error
-print(account1.__balance) # Uncommenting this line will raise an AttributeError
+print(account1.__balance)  # Uncommenting this line will raise an AttributeError
 
 # Abstraction in Python
 # abstract is a process of hiding the implementation details and showing only functionality to the user.
 # we can achieve abstraction using abstract classes and methods
 
 # here we are creating an abstract class Shape with an abstract method area
-from abc import ABC, abstractmethod # here abc is abstract base class, ABC is a module, abstractmethod is a decorator, we use it to define abstract methods, which are methods that have a declaration but do not have an implementation, we cannot create objects of abstract classes, we need to create a derived class that implements the abstract methods, then we can create objects of the derived class, and use the methods defined in the abstract class, we use abstraction to hide the complexity of the system and show only the essential features to the user
+from abc import ABC, \
+    abstractmethod  # here abc is abstract base class, ABC is a module, abstractmethod is a decorator, we use it to define abstract methods, which are methods that have a declaration but do not have an implementation, we cannot create objects of abstract classes, we need to create a derived class that implements the abstract methods, then we can create objects of the derived class, and use the methods defined in the abstract class, we use abstraction to hide the complexity of the system and show only the essential features to the user
+
+
 class Shape(ABC):
     @abstractmethod
     def area(self):
         pass
+
+
 class Rectangle(Shape):
     def __init__(self, width, height):
         self.width = width
         self.height = height
+
     def area(self):
         return self.width * self.height
-
-
-
