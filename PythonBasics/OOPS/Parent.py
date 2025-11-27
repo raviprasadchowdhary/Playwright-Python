cb1b4f84364e06f811e298465a36c5784ba32c83
Raviprasad <78257726+raviprasadchowdhary@users.noreply.github.com>
2025-11-27 21:26:33 +0530

    oops

---
+++ PythonBasics/OOPS/Parent.py
@@ -0,0 +1,17 @@
+class BaseCarModel:
+    price = 1000000 # base price of the car model
+
+    def __init__(self, model_name, year):
+        self.model_name = model_name
+        self.year = year
+
+    def display_info(self):
+        return f"Model: {self.model_name}; Year: {self.year}; Base price: ${BaseCarModel.price}"
+
+# Example usage:
+car1 = BaseCarModel("Sedan LX", 2025)
+
+print(f"Base price of the car is: ${car1.price}")
+print(car1.display_info())
+
+
