103e019581ffdda157099e6ebc00d75ac57689b1
Raviprasad <78257726+raviprasadchowdhary@users.noreply.github.com>
2025-11-30 14:40:22 +0530

    test_core_locators

--- Playwright/test_coreLocators.py
+++ Playwright/test_coreLocators.py
@@ -2,13 +2,16 @@ import time
 
 from playwright.sync_api import Page
 
-def test_core_locators_getByLabelAndRole(page:Page):
+def test_core_locators(page:Page):
     page.goto("https://rahulshettyacademy.com/loginpagePractise/")
     # labels
     page.get_by_label("Username:").fill("rahulshettyacademy")
     page.get_by_label("Password:").fill("learning")
+    # css selectors
+    page.locator("#terms").check()
+    time.sleep(3)
     # roles
     page.get_by_role("combobox").select_option("consult")
     # print(page.get_by_role("button"))
     page.get_by_role("button", name="Sign In").click()
-    time.sleep(10)
+    time.sleep(3)
