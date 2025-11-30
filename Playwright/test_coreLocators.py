9e6eb57184d9ec5305577f28738d889bc1bd1b14
Raviprasad <78257726+raviprasadchowdhary@users.noreply.github.com>
2025-11-30 15:00:52 +0530

    auto-wait

--- Playwright/test_coreLocators.py
+++ Playwright/test_coreLocators.py
@@ -1,17 +1,18 @@
 import time
 
-from playwright.sync_api import Page
+from playwright.sync_api import Page, expect
+
 
 def test_core_locators(page:Page):
     page.goto("https://rahulshettyacademy.com/loginpagePractise/")
     # labels
     page.get_by_label("Username:").fill("rahulshettyacademy")
-    page.get_by_label("Password:").fill("learning")
+    page.get_by_label("Password:").fill("learning1")
     # css selectors
     page.locator("#terms").check()
-    time.sleep(3)
     # roles
     page.get_by_role("combobox").select_option("consult")
     # print(page.get_by_role("button"))
     page.get_by_role("button", name="Sign In").click()
-    time.sleep(3)
+    # Automatic wait
+    expect(page.get_by_text("Incorrect username/password.")).to_be_visible()
