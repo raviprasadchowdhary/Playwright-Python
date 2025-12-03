937e2bdee5feb42e936604a832cb1f05d0980dd9
Raviprasad <78257726+raviprasadchowdhary@users.noreply.github.com>
2025-12-03 23:33:32 +0530

    test_iframe_handling

--- Playwright/test_moreValidations.py
+++ Playwright/test_moreValidations.py
@@ -15,3 +15,11 @@ def test_alerts_dialog(page:Page):
     # handle alert
     page.on("dialog", lambda dialog: dialog.accept())
     page.get_by_role("button", name="Confirm").click()
+
+def test_iframe_handling(page:Page):
+    page.goto("https://rahulshettyacademy.com/AutomationPractice/")
+    # handle iframe
+    frame = page.frame_locator("#courses-iframe")
+    # frame.get_by_role("link", name="Learning Paths").click()
+    frame.locator("a.text-muted-foreground[href='/learning-paths']").nth(0).click()
+    expect(frame.locator("body")).to_contain_text("Learning Journey")
