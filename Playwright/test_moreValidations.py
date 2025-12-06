bb9a271a6e466a0bbdadb5204b5694155789b598
Raviprasad <78257726+raviprasadchowdhary@users.noreply.github.com>
2025-12-06 12:44:48 +0530

    test_validateTableContent

--- Playwright/test_moreValidations.py
+++ Playwright/test_moreValidations.py
@@ -1,3 +1,5 @@
+import time
+
 from playwright.sync_api import Page, expect
 
 
@@ -23,3 +25,20 @@ def test_iframe_handling(page:Page):
     # frame.get_by_role("link", name="Learning Paths").click()
     frame.locator("a.text-muted-foreground[href='/learning-paths']").nth(0).click()
     expect(frame.locator("body")).to_contain_text("Learning Journey")
+
+def test_validateTableContent(page:Page):
+    # check the price of Rice is 37
+    # identify price column
+    # identify rice row
+    # extract the price value
+    page.goto("https://rahulshettyacademy.com/seleniumPractise/#/offers")
+
+    for i in range(page.locator("th").count()):
+        if page.locator("th").nth(i).filter(has_text="Price").count()>0:
+            columnIndex = i
+            print(f"Price column index is: {columnIndex}")
+            break
+
+    price = page.locator("tr").filter(has_text="Rice").locator("td").nth(columnIndex).text_content()
+    print(f"Price of Rice is: {price}")
+    assert price == "37"
