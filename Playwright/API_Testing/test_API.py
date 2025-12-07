910fc3f270eb014dbaf44e44ea63d62bceaf3662
Raviprasad <78257726+raviprasadchowdhary@users.noreply.github.com>
2025-12-07 22:19:40 +0530

    test_addToCart

--- Playwright/API_Testing/test_API.py
+++ Playwright/API_Testing/test_API.py
@@ -6,3 +6,11 @@ def test_getAuthToken(playwright):
 
     assert getAuthToken(playwright) is not None
 
+def test_addToCart(playwright):
+    print(f"\nexecution of addToCart is started...")
+    print(f"Status code is: {addToCartZaraCoat3(playwright).status}")
+    print(f"\nResponse body is: \n{addToCartZaraCoat3(playwright).json()}")
+
+    assert addToCartZaraCoat3(playwright).status == 200
+    assert addToCartZaraCoat3(playwright).json()["message"] == "Product Added To Cart"
+
