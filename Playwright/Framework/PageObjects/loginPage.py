d7723e4f350ef87131499743a430a427027b58d2
Raviprasad <78257726+raviprasadchowdhary@users.noreply.github.com>
2025-12-14 08:50:15 +0530

    login method

--- Playwright/Framework/PageObjects/loginPage.py
+++ Playwright/Framework/PageObjects/loginPage.py
@@ -2,6 +2,11 @@ class LoginPage:
     def __init__(self, page):
         self.page = page
 
-    # @staticmethod
+    # methods
     def navigate(self):
         self.page.goto("https://rahulshettyacademy.com/client")
+
+    def login(self, test_credentials_list):
+        self.page.get_by_placeholder("email@example.com").fill(test_credentials_list["username"])
+        self.page.get_by_placeholder("enter your passsword").fill(test_credentials_list["password"])
+        self.page.get_by_role("button", name="Login").click()
