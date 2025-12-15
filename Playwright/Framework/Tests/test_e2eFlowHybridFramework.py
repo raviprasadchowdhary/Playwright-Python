75a803d431b74f61de0d1cfba314bdee5f3ba411
Raviprasad <78257726+raviprasadchowdhary@users.noreply.github.com>
2025-12-15 21:15:04 +0530

    updated test function with coomon method useage

--- Playwright/Framework/Tests/test_e2eFlowHybridFramework.py
+++ Playwright/Framework/Tests/test_e2eFlowHybridFramework.py
@@ -2,7 +2,9 @@ import json
 
 import pytest
 from playwright.sync_api import Playwright, expect
+from pytest_playwright.pytest_playwright import page
 
+from Playwright.Framework.PageObjects.common import Common
 from Playwright.Framework.PageObjects.loginPage import LoginPage
 from utils.apiBase import APIUtils
 
@@ -13,12 +15,14 @@ print(f"Test data loaded from json file: {test_data}")
 
 
 @pytest.mark.parametrize("test_credentials_list", test_credentials)
-def test_e2eTest_createOrderAndVerify(playwright: Playwright, test_credentials_list):
-    browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context()
-    page = context.new_page()
-
+def test_e2eTest_createOrderAndVerify(playwright: Playwright, test_credentials_list, page):
+    # browser = playwright.chromium.launch(headless=False)
+    # context = browser.new_context()
+    # page = context.new_page()
     orderId = APIUtils.createOrder(playwright, userCredentials=test_credentials_list)
+
+    common = Common(playwright)
+    page = common.create_browser_context("chrome", "False")
 
     loginPage = LoginPage(page)
     loginPage.navigate()
