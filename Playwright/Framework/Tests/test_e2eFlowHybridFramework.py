683c2f65da271adf77a3257d8bf0bc7c83c99cb8
Raviprasad <78257726+raviprasadchowdhary@users.noreply.github.com>
2025-12-15 21:27:15 +0530

    refactor

--- Playwright/Framework/Tests/test_e2eFlowHybridFramework.py
+++ Playwright/Framework/Tests/test_e2eFlowHybridFramework.py
@@ -16,9 +16,6 @@ print(f"Test data loaded from json file: {test_data}")
 
 @pytest.mark.parametrize("test_credentials_list", test_credentials)
 def test_e2eTest_createOrderAndVerify(playwright: Playwright, test_credentials_list, page):
-    # browser = playwright.chromium.launch(headless=False)
-    # context = browser.new_context()
-    # page = context.new_page()
     orderId = APIUtils.createOrder(playwright, userCredentials=test_credentials_list)
 
     common = Common(playwright)
