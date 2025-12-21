3968704c0404f94c08875c8f000eb05a2cfca90b
Raviprasad <78257726+raviprasadchowdhary@users.noreply.github.com>
2025-12-21 18:04:37 +0530

    fix data

--- Playwright/E2E/test_e2eFlowHybrid.py
+++ Playwright/E2E/test_e2eFlowHybrid.py
@@ -13,7 +13,7 @@ def test_createOrderAndVerify(playwright: Playwright):
 
     # login
     page.goto("https://rahulshettyacademy.com/client")
-    page.get_by_placeholder("email@example.com").fill("RahulKumar@gmail.com")
+    page.get_by_placeholder("email@example.com").fill("RahulChowdhary@gmail.com")
     page.get_by_placeholder("enter your passsword").fill("Rahul@123")
     page.get_by_role("button", name="Login").click()
 
