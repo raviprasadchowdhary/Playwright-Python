249b704ce6d02f498b3f25923eb7decaaa8f79ad
Raviprasad <78257726+raviprasadchowdhary@users.noreply.github.com>
2025-12-21 17:45:22 +0530

    created a copy of apibase

---
+++ utils/apiBase.py
@@ -0,0 +1,39 @@
+from typing import Any
+
+from playwright.sync_api import Playwright
+
+createOrderRequestBody = {
+    "orders": [
+        {
+            "country": "India",
+            "productOrderedId": "68a961459320a140fe1ca57a"
+        }
+    ]
+}
+
+
+class APIUtils:
+    @staticmethod
+    def getToken(playwright: Playwright,):
+        api_request_context = playwright.request.new_context(base_url="https://rahulshettyacademy.com",
+                                                             ignore_https_errors=True)
+        response = api_request_context.post(url="/api/ecom/auth/login"
+                                            , data={
+                "userEmail": "RahulChowdhary@gmail.com",
+                "userPassword": "Rahul@123"
+            })
+        response_body = response.json()
+        print(f"\n Login/getToken response body is: \n{response_body}")
+        return response_body["token"]
+
+    @staticmethod
+    def createOrder(playwright: Playwright) -> Any:
+        api_request_context = playwright.request.new_context(base_url="https://rahulshettyacademy.com",
+                                                             ignore_https_errors=True)
+        response = api_request_context.post(url="/api/ecom/order/create-order"
+                                            , data=createOrderRequestBody
+                                            , headers={"Authorization": APIUtils.getToken(playwright),
+                                                       "Content-type": "application/json"})
+        responseBody = response.json()
+        print(f"\n Create order response body: \n{responseBody}")
+        return responseBody["orders"][0]
